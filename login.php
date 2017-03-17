a6d42711c42e794493f86689517bffe9ef4c98c0
Jacky <jackiechan19891016@outlook.com>
2017-03-17 10:17:14 +0400

    log in feature completed sign up save data of user in database didn't create session yet

--- login.php
+++ login.php
@@ -2,26 +2,75 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 <?php
+  require('db_connect.php');
+
   if(isset($_POST['create'])) {
-    header("Location:timetable.php"); 
+    $password = crypt($_POST['password']);
+    $sql = "
+      CREATE TABLE IF NOT EXISTS users
+      (
+        id int NOT NULL AUTO_INCREMENT,
+        name varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        password TEXT,
+        level int NOT NULL,
+        PRIMARY KEY (ID)
+      );
+
+      INSERT INTO `timetable`.`users` (`name`, `email`, `password`, `level`) VALUES ('{$_POST['name']}', '{$_POST['email']}', '$password', 2);
+    ";
+
+    if ($conn->multi_query($sql) != FALSE) {
+      header("Location:timetable.php"); 
+    }
+    else {
+      echo($conn->error);
+    }
   }
   if(isset($_POST['login'])) {
-    header("Location:timetable.php"); 
+    $sql = "
+      SELECT * FROM `timetable`.`users` WHERE email='{$_POST['email']}';
+    ";
+
+    $result = $conn->query($sql);
+
+    if ( $result->num_rows > 0 ) {
+      list($id, $name, $email, $password, $level) = $result -> fetch_array(MYSQLI_BOTH);
+
+      if (crypt($_POST['password'], $password) === $password) {
+        $_SESSION['user_id'] = $id;
+        $_SESSION['user_name'] = $id;
+        $_SESSION['user_email'] = $email;
+        $_SESSION['user_password'] = $password;
+        $_SESSION['user_level'] = $level;
+        if ($level == 1) {
+          header("Location:admin_panel.php");
+        }
+        elseif ($level == 2) {
+          header("Location:timetable.php");
+        }
+        
+      }
+      else {
+        echo "Email or password is incorrect. Please try again.";
+      }
+    }
   }
+
 ?>
 
 <div class="login-page">
   <div class="form">
     <form class="register-form" action="login.php" method="post">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
+      <input type="text" name="name" placeholder="name"/>
+      <input type="password" name="password" placeholder="password"/>
+      <input type="email" name="email" placeholder="email address"/>
       <button name="create">create</button>
       <p class="message">Already registered? <a href="#">Sign In</a></p>
     </form>
     <form class="login-form" action="login.php" method="post">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
+      <input type="email" name="email" placeholder="email"/>
+      <input type="password" name="password" placeholder="password"/>
       <button name="login">login</button>
       <p class="message">Not registered? <a href="#">Create an account</a></p>
     </form>
